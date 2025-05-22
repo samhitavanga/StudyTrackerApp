34a985fb0f43c37e4b82f57fd45c52cd6f5d0427
Samhita <samhita@MacBook-Pro-132.local>
2025-05-22 09:12:42 -0700

    Fix: Remove dependency on non-existent env-config.js in server.js

--- backend/netlify/functions/server.js
+++ backend/netlify/functions/server.js
@@ -1,11 +1,7 @@
 const path = require('path');
 const strapiDir = path.resolve(__dirname, '../../');
-const envConfig = require('../../env-config');
 
-// Set environment variables from our config
-Object.keys(envConfig).forEach(key => {
-  process.env[key] = envConfig[key];
-});
+// Environment variables are already set by Netlify
 
 // Point to the Strapi application
 process.chdir(strapiDir);
